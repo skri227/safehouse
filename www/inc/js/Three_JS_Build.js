7a5d9a270369bfe4618292f56b0587f89705a55a
Vitara04 <alexisspahn@gmail.com>
2017-04-24 18:43:47 -0400

    map checker

--- www/inc/js/Three_JS_Build.js
+++ www/inc/js/Three_JS_Build.js
@@ -436,7 +436,7 @@ function drawBoard(){
 
 	var boardMesh = new THREE.PlaneGeometry(mapSizeX,mapSizeZ); //7 wide, 3.4 tall - close to tabletop size
 	THREE.ImageUtils.crossOrigin = '';
-	var mapOverlay = THREE.ImageUtils.loadTexture('http://i.imgur.com/sM7pySL.jpg');
+	var mapOverlay = THREE.ImageUtils.loadTexture('http://i.imgur.com/uIe0fpg.jpg');
 	var boardMaterial = new THREE.MeshBasicMaterial({map: mapOverlay})
 	var board = new THREE.Mesh(boardMesh,boardMaterial);
 	board.rotation.x = (-90*Math.PI/180);
